949569501c2c4c345b13eb42063004e27cea0c44
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-07-21 23:01:54 +0200

    Added the nav to the video and working on the video player

---
+++ resources/js/video-player.js
@@ -0,0 +1,138 @@
+const container = document.querySelector('.video-container')
+const playPauseBtn = document.querySelector('.play-pause-btn');
+const miniPlayerBtn = document.querySelector('.mini-player-btn');
+const theaterBtn = document.querySelector('.theater-btn');
+const fullScreenBtn = document.querySelector('.full-screen-btn');
+const muteBtn = document.querySelector('.mute-btn');
+const volumeSlider = document.querySelector('.volume-slider');
+const currentTime = document.querySelector('.current-time');
+const totalTime = document.querySelector('.total-time');
+const video = document.querySelector('video');
+
+document.addEventListener('keydown', e=>{
+    const tagName = document.activeElement.tagName.toLowerCase();
+    if(tagName==="input") return;
+    switch(e.key.toLowerCase())
+    {
+        case " ":
+                if(tagName==="button") return;
+            case "k":
+                togglePlay();
+                break;
+            case "i":
+                toggleMiniPlayer();
+                break;
+            case "t":
+                toggleTheater();
+                break;
+            case "f":
+                toggleFullScreen();
+                break;
+            case "m":
+                toggleMute();
+                break;
+            /*case "arrowleft":
+                case "j":
+                    skip(-5);
+                    break;
+            case "arrowright":
+                case "l":
+                    skip(5);
+                    break;*/
+    }
+});
+
+playPauseBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay)
+
+video.addEventListener('play', ()=>container.classList.remove('paused'));
+video.addEventListener('pause', ()=>container.classList.add('paused'));
+
+document.addEventListener('fullscreenchange',()=>container.classList.toggle('full-screen', document.fullscreenElement));
+
+//volume listeners
+muteBtn.addEventListener('click', toggleMute);
+video.addEventListener('volumechange', ()=>{
+    volumeSlider.value = video.volume;
+    let volumeLevel;
+    if(video.muted || video.volume===0)
+    {
+        volumeSlider.value = 0;
+        volumeLevel = "muted";
+    }
+    else if (video.volume>.5) volumeLevel = "high"; 
+    else volumeLevel = "low";
+
+    container.dataset.volumeLevel = volumeLevel;
+        
+});
+volumeSlider.addEventListener('input', e=>{
+    video.volume = e.target.value;
+    video.muted = e.target.value===0;
+});
+
+
+//view mods listeners
+miniPlayerBtn.addEventListener('click', toggleMiniPlayer);
+theaterBtn.addEventListener('click', toggleTheater);
+fullScreenBtn.addEventListener('click', toggleFullScreen);
+video.addEventListener('enterpictureinpicture', ()=>container.classList.add('mini-player'));
+video.addEventListener('leavepictureinpicture', ()=>container.classList.remove('mini-player'));
+
+//duration listeners
+video.addEventListener('loadeddata', ()=>totalTime.textContent=formatDuration(video.duration));
+video.addEventListener('timeupdate', ()=>{
+    currentTime.textContent = formatDuration(video.currentTime);
+})
+
+//view mods
+function togglePlay()
+{
+    video.paused ? video.play() : video.pause();
+}
+
+function toggleMiniPlayer()
+{
+    container.classList.contains('mini-player')
+        ? document.exitPictureInPicture()
+        : video.requestPictureInPicture();
+}
+
+function toggleTheater()
+{
+    container.classList.toggle('theater');
+}
+
+function toggleFullScreen()
+{
+    document.fullscreenElement==null 
+        ? container.requestFullscreen()
+        : document.exitFullscreen()
+}
+
+//volume
+function toggleMute()
+{
+    video.muted = !video.muted;
+}
+
+//duration
+
+function formatDuration(t)
+{
+    const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
+        minimumIntegerDigits: 2
+    });
+    const s = Math.floor(t%60);
+    const m = Math.floor(t/60)%60;
+    const h = Math.floor(t/3600);
+
+    return h===0
+        ? `${m}:${leadingZeroFormatter.format(s)}`
+        : `${h}:${m}:${leadingZeroFormatter.format(s)}`;
+}
+
+/*function skip(t)
+{
+    video.currentTime+=t;
+}*/
--- resources/views/post/home.blade.php
+++ resources/views/post/home.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LibreIo / {{$title}}</title>
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/css/video-player.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/css/video-player.css','resources/js/app.js' , 'resources/js/video-player.js'])
 </head>
 <body class="">
+    @include('layouts.navigation')
     <x-video-player/>
 </body>
 </html>
