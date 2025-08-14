0c19dafa31d140a29a2ecc68924fb20ccae04e1b
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-08-14 19:00:50 +0200

    Fixed the formating a bit. I will try to implement the infinite scroll in the homepage rn

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                     
+                     <div class="mt-5 grid gap-4 
+                            grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                        
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/profile/home.blade.php
+++ resources/views/profile/home.blade.php
@@ -68,42 +68,41 @@
                 </div>
                 {{-- navbar user --}}
                 <div class="flex flex-row gap-4">
-<!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
-<nav class="relative bg-white after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div class="relative flex h-16 items-center justify-between">
-      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        <!-- Mobile menu button-->
-        <button type="button" command="--toggle" commandfor="mobile-menu" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
-          <span class="absolute -inset-0.5"></span>
-          <span class="sr-only">Open main menu</span>
-          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 in-aria-expanded:hidden">
-            <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
-          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 not-in-aria-expanded:hidden">
-            <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
-          </svg>
-        </button>
-      </div>
-        @if($username===Auth::user()->name)
-        <div class="hidden sm:ml-6 sm:block">
-          <div class="flex space-x-4">
-            <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-500 hover:bg-white/5 hover:text-white" -->
-            <a href="{{ route('profile.home', ['username' => $username]) }}" aria-current="page" class="rounded-md text-sm px-3 py-2 font-medium {{ $action==="" ? 'bg-gray-950/50  text-white' :  'text-gray-500 hover:bg-white/5 transition-colors hover:text-black'}}">
-                Home
-            </a>
-            <a href="{{ route('profile.home', ['username'=>$username, 'action'=>'private']) }}" class="rounded-md px-3 py-2 text-sm font-medium {{ $action==="private" ? 'bg-gray-950/50  text-white' :  'text-gray-500 hover:bg-white/5 transition-colors hover:text-black'}}">
-                Private
-            </a>
-          </div>
-        </div>
-        @endif
-      </div>
-    </div>
-  </div>
-</nav>
+                  <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
+                  <nav class="relative bg-white after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
+                    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                      <div class="relative flex h-16 items-center justify-between">
+                        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                          <!-- Mobile menu button-->
+                          <button type="button" command="--toggle" commandfor="mobile-menu" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+                            <span class="absolute -inset-0.5"></span>
+                            <span class="sr-only">Open main menu</span>
+                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 in-aria-expanded:hidden">
+                              <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 not-in-aria-expanded:hidden">
+                              <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                          </button>
+                        </div>
+                          @if($username===Auth::user()->name)
+                          <div class="hidden sm:ml-6 sm:block">
+                            <div class="flex space-x-4">
+                              <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-500 hover:bg-white/5 hover:text-white" -->
+                              <a href="{{ route('profile.home', ['username' => $username]) }}" aria-current="page" class="rounded-md text-sm px-3 py-2 font-medium {{ $action==="" ? 'bg-gray-950/50  text-white' :  'text-gray-500 hover:bg-white/5 transition-colors hover:text-black'}}">
+                                  Home
+                              </a>
+                              <a href="{{ route('profile.home', ['username'=>$username, 'action'=>'private']) }}" class="rounded-md px-3 py-2 text-sm font-medium {{ $action==="private" ? 'bg-gray-950/50  text-white' :  'text-gray-500 hover:bg-white/5 transition-colors hover:text-black'}}">
+                                  Private
+                              </a>
+                            </div>
+                          </div>
+                          @endif
+                        </div>
+                      </div>
+                    </div>
+                  </nav>
                 </div>
-
                 <div class="mt-5 grid gap-4 
                         grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                     @foreach ($videos as $video)
