0a032bf65fe548857b394b07df49a1572769ba96
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-08-17 22:13:45 +0200

    added css and js files to vite config js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/loader.css',
+                'resources/css/video-player.css',
+                'resources/js/app.js',
+                'resources/js/date-handler.js',
+                'resources/js/faq-handler.js',
+                'resources/js/follow-handler.js',
+                'resources/js/infiniteScroll.js',
+                'resources/js/like-handler.js',
+                'resources/js/number-handler.js',
+                'resources/js/save-handler.js',
+                'resources/js/video-player.js',
+            ],
             refresh: true,
         }),
     ],
