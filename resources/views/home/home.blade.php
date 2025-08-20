682f4f8ced8c43f0ac01e25c54a9611bfafc0893
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-08-20 15:49:49 +0200

    Moved the posts grid to it's own place

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -9,19 +9,5 @@
         </div>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <div id="posts-list" class="mt-5 grid gap-4 
-                            grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                        @include('partials.posts', ['posts'=>$posts])
-                    </div>
-                    <div class="flex w-full justify-center mt-5">
-                        {{ $posts->links() }}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('home.partials.posts-grid', ['posts' => $posts])
 </x-app-layout>
