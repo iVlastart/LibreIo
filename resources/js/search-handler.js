c8431aba813cea452a83215342381d7a248d70b9
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-08-20 16:45:42 +0200

    Added enter key search functionality to the search bar

--- resources/js/search-handler.js
+++ resources/js/search-handler.js
@@ -1,7 +1,16 @@
 const searchbtn = document.getElementById('search-btn');
 const searchbar = document.getElementById('search-bar');
 
+searchbar.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') redirect();
+});
+        
 searchbtn.addEventListener('click', ()=>{
+    redirect();
+});
+
+function redirect()
+{
     if(searchbar.value!=='')
         location.assign('/search/'+encodeURIComponent(searchbar.value));
-});
+}
