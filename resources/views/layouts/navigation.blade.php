277a6bf5ed277352d7211c14cafa507037027757
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-08-20 16:08:45 +0200

    Adding the svgs to the navbar. Also made the logout door icon to open on hover

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -38,10 +38,21 @@
                     </x-slot>
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.home', ['username' => Auth::user()->name])">
-                            {{ __('Profile') }}
+                            <div class="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                                </svg>&nbsp;
+                                {{ __('Profile') }}
+                            </div>
                         </x-dropdown-link>
                         <x-dropdown-link :href="route('post.create')" :active="request()->routeIs('post.create')">
-                            {{ __('Create') }}
+                            <div class="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+                                </svg>&nbsp;
+                                {{ __('Create') }}
+                            </div>
+                            
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -51,7 +62,20 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                <div class="flex items-center group">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="flex group-hover:hidden" 
+                                            viewBox="0 0 16 16">
+                                        <path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2"/>
+                                    </svg>
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="hidden group-hover:flex" viewBox="0 0 16 16">
+                                        <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15zM11 2h.5a.5.5 0 0 1 .5.5V15h-1zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1"/>
+                                    </svg>
+                                    <div class="flex items-center text-red-500">
+                                        {{ __('Log Out') }}
+                                    </div>
+                                </div>
+                                
                             </x-dropdown-link>
                         </form>
                     </x-slot>
