2f98efdddc51320ea2b047c3cdc267f045d2f5b4
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-08-22 14:57:44 +0200

    Rolled back the provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Log;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,23 +26,5 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
-
-        VerifyEmail::createUrlUsing(function ($notifiable) {
-    $url = URL::temporarySignedRoute(
-        'verification.verify',
-        now()->addHours(6),
-        [
-            'id'   => $notifiable->getKey(),
-            'hash' => sha1($notifiable->getEmailForVerification()),
-        ]
-    );
-
-    Log::info('Generated verification URL', [
-        'url' => $url,
-        'app_key' => config('app.key'),
-    ]);
-
-    return $url;
-});
     }
 }
