0ffc73fed47a849a1cc185050fa986b126391262
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-08-24 23:04:41 +0200

    Made the form where you can submit the project name

--- resources/views/editor/create.blade.php
+++ resources/views/editor/create.blade.php
@@ -1,9 +1,24 @@
-<x-app-layout>
+<x-app-layout class="relative">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Editor') }}
+            {{ __('Name your project') }}
         </h2>
     </x-slot>
 
+    <div class="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 p-6">
+        <form method="POST" action="{{ route('editor.create') }}" class="max-w-2xl mx-auto mt-10">
+            @csrf
+
+            <div class="mb-4">
+                <x-input-label for="Name" :value="__('Name')"/>
+                <x-text-input name="name" id="Name" class="block mt-1 w-full" type="text" :value="__('')" maxlength="30"/>
+                <x-input-error :messages="$errors->get('name')" class="mt-2"/>
+            </div>
+
+            <div class="flex justify-center">
+                <button type="submit" class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition-colors duration-300">Create Project</button>
+            </div>
+        </form>
+    </div>
     
 </x-app-layout>
