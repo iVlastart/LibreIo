dd2e4df5c31d0b543d9a32854649afc08ab3a59a
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-08-25 23:02:06 +0200

    Fixed a something minor with the collapsing and also added the functionality to show the other ones

--- resources/js/editor/eLeftSide-handler.js
+++ resources/js/editor/eLeftSide-handler.js
@@ -2,14 +2,42 @@ import $ from 'jquery';
 
 //script to handle the left side of the video editor 
 $(()=>{
+    let collapsed = false;
+    let activeElem = "dropzone";
     const dropzone = $('#dropzone');
     const nav = $('#nav');
     const collapseSvg = $('#collapseSvg');
     const showSvg = $('#showSvg');
+    const uploads = $('#uploads');
+    const subtitles = $('#subtitles');
     $('#collapseBtn').on('click', ()=>{
-        dropzone.toggleClass('hidden');
-        nav.toggleClass('hidden');
+       collapsed
+            ? $(`#${activeElem}`).removeClass('hidden')
+            : $(`#${activeElem}`).addClass('hidden');
         collapseSvg.toggleClass('hidden');
-        showSvg.toggleClass('hidden')
-    })
+        showSvg.toggleClass('hidden');
+        nav.toggleClass('hidden');
+        collapsed = !collapsed;
+    });
+
+    $('#dropzoneBtn').on('click', ()=>{
+        dropzone.removeClass('hidden');
+        uploads.addClass('hidden');
+        subtitles.addClass('hidden');
+        activeElem = "dropzone";
+    });
+
+    $('#uploadsBtn').on('click', ()=>{
+        dropzone.addClass('hidden');
+        uploads.removeClass('hidden');
+        subtitles.addClass('hidden');
+        activeElem = "uploads";
+    });
+
+    $('#subtitlesBtn').on('click', ()=>{
+        dropzone.addClass('hidden');
+        uploads.addClass('hidden');
+        subtitles.removeClass('hidden');
+        activeElem = "subtitles";
+    });
 });
--- resources/views/editor/edit.blade.php
+++ resources/views/editor/edit.blade.php
@@ -15,7 +15,6 @@
                     <button id="subtitlesBtn" class="hover:scale-105 transition-transform duration-300">Subtitles</button>
                 </div>
                 
-
                 <button class="mx-auto" title="toggle" id="collapseBtn" class="flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" id="collapseSvg" class="bi bi-arrow-left" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
@@ -35,6 +34,15 @@
                 <input type="file" name="file" class="absolute inset-0 w-full h-full opacity-0 cursor-pointer" />
                 <p>Drag and drop your files here</p>
             </form>
+
+            <div class="w-full h-full mt-3 flex items-center justify-center hidden
+                    border-r-4 border-gray-400" id="uploads">
+                Uploads
+            </div>
+
+            <div class="w-full h-full mt-3 flex items-center justify-center hidden" id="subtitles">
+                Subtitles
+            </div>
         </div>
     </div>
 
